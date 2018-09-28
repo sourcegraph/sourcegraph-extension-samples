198d245df9ddd1794f079e590a0761b770149476
Chris Wendt <chrismwendt@gmail.com>
2018-09-28 15:43:03 -0700

    feat: add npm-import-stats sample

---
+++ npm-import-stats/README.md
@@ -0,0 +1,5 @@
+# npm import stats [Sourcegraph extension](https://github.com/sourcegraph/sourcegraph-extension-api)
+
+This example extension will display a link to the npm package next to import statements in JavaScript/TypeScript.
+
+![](demo.gif)
---
+++ npm-import-stats/src/extension.ts
@@ -0,0 +1,51 @@
+import * as sourcegraph from "sourcegraph";
+import { Observable, from, EMPTY } from "rxjs";
+import { ajax } from "rxjs/ajax";
+import { map, concatMap, toArray } from "rxjs/operators";
+
+const fetchDownloads = (pkg: string): Observable<number> => {
+  return ajax({
+    url: `https://api.npmjs.org/downloads/point/last-week/${pkg}`
+  }).pipe(
+    map(v => {
+      return v && v.response && v.response.downloads ? v.response.downloads : 0;
+    })
+  );
+};
+
+export function activate(): void {
+  sourcegraph.workspace.onDidOpenTextDocument.subscribe(doc => {
+    from(doc.text.split("\n"))
+      .pipe(
+        concatMap((line, lineNumber) => {
+          const match = /^import.*from .(\w+)/.exec(line);
+          if (match && match.length > 1) {
+            const pkg = match[1];
+            return fetchDownloads(pkg).pipe(
+              map(downloads => ({ downloads, lineNumber, pkg }))
+            );
+          } else {
+            return EMPTY;
+          }
+        }),
+        toArray()
+      )
+      .subscribe(annotations => {
+        sourcegraph.app.activeWindow.visibleViewComponents[0].setDecorations(
+          null,
+          annotations.map(({ downloads, lineNumber, pkg }) => ({
+            range: new sourcegraph.Range(
+              new sourcegraph.Position(lineNumber, 0),
+              new sourcegraph.Position(lineNumber, 0)
+            ),
+            after: {
+              contentText: " View on npm (" + downloads + " DLs last week)",
+              linkURL: `https://www.npmjs.com/package/${pkg}`,
+              backgroundColor: "pink",
+              color: "black"
+            }
+          }))
+        );
+      });
+  });
+}
