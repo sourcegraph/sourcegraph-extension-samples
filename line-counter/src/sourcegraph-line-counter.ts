949871f13c5d3417fa1fe6a58765bb2721231842
Ryan Blunden <ryan.blunden@gmail.com>
2018-11-30 13:21:12 -0800

    Minor updates

--- line-counter/src/sourcegraph-line-counter.ts
+++ line-counter/src/sourcegraph-line-counter.ts
@@ -1,20 +1,35 @@
 import * as sourcegraph from 'sourcegraph'
 
+/**
+ * Returns a CodeEditor instance (if a Window is active) that provides access to
+ * the document being viewed.
+ */
 function activeEditor(): sourcegraph.CodeEditor | undefined {
     return sourcegraph.app.activeWindow
         ? sourcegraph.app.activeWindow.visibleViewComponents[0]
         : undefined
 }
 
-export function activate(): void {
-  sourcegraph.commands.registerCommand('linecounter.displayLineCount', (editor: sourcegraph.CodeEditor | undefined = activeEditor()) => {
+/**
+ * Display a notification with the filename and line count of the document being viewed.
+ * @param editor
+ */
+function displayLineCount(editor: sourcegraph.CodeEditor | undefined = activeEditor()): void {
     if(!editor) {
-        return;
+        return
     }
 
     const lineCount = editor.document.text.split(/\n/).length - 1
     const fileName = editor.document.uri.substring(editor.document.uri.lastIndexOf('/') + 1).split('#').slice(-1)[0]
+    const message = `The ${fileName} file has ${lineCount} line${lineCount > 1 ? 's' : ''} of code `
 
-    sourcegraph.app.activeWindow!.showNotification(`The ${fileName} file has ${lineCount} line${lineCount > 1 ? 's' : ''} of code `)
-  })
+    sourcegraph.app.activeWindow!.showNotification(message)
+}
+
+/**
+ * The activate function is called when one of the extensions `activateEvents`
+ * conditions in package.json are satisfied.
+ */
+export function activate(): void {
+    sourcegraph.commands.registerCommand('linecounter.displayLineCount', () => displayLineCount())
 }
